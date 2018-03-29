b0088729c88a7b0a218a5d681e0609f976b2808d
liangjunljlj <453021745@qq.com>
2018-03-29 18:43:27 +0800

    中英文切换

--- src/components/menus/index.js
+++ src/components/menus/index.js
@@ -1,8 +1,14 @@
 import React, {PureComponent} from "react";
 import {I18n, Trans} from "react-i18next";
 import {NavLink, Link} from "react-router-dom";
+import { indexRemFun, setLocalItem } from "../../utils/util";
 import logo from "../../assets/images/21.png";
 import "./index.less";
+const change = (lng) => {
+  setLocalItem("language",lng);
+  window.i18n.changeLanguage(lng);
+}
+
 function Menus(props){
 	const { lng } = props;
 	return (
@@ -10,19 +16,19 @@ function Menus(props){
           {(t, {i18n}) => (
             <div className="menu">
 							<ul>
-								<li>Home</li>
-								<li>About</li>
+								<li>{t('menus.txt1',lng)}</li>
+								<li>{t('menus.txt2',lng)}</li>
 								<li className="logo">
 									<img src={logo} alt="logo" />
 								</li>
 								<li className="rl end">
-									Language
+									{t('menus.txt3',lng)}
 									<div className="sub-menu">
-										<p>English</p>
-										<p>中文</p>
+										<p onClick={change.bind(this,"en")}>English</p>
+										<p onClick={change.bind(this,"zh")}>中文</p>
 									</div>
 								</li>
-								<li className="rl">Contact</li>
+								<li className="rl">{t('menus.txt4',lng)}</li>
 							</ul>
 						</div>
           )}
--- src/entries/about/components/root/index.js
+++ src/entries/about/components/root/index.js
@@ -7,7 +7,7 @@ import Footer from "../../../../components/footer/";
 import bglf from "../../../../assets/images/1.png";
 import bgrt from "../../../../assets/images/6.png";
 import "./index.less";
-import { indexRemFun } from "../../../../utils/util";
+import { indexRemFun, setLocalItem } from "../../../../utils/util";
 
 
 
@@ -26,39 +26,32 @@ export default class Root extends PureComponent {
         });
     }
     render() {
-        const { lng } = this.props;
-				let lang = window.i18n.language;
-				lang = "zh"
+        let { lng } = this.props;
+       // setLocalItem("language",lng);
+       // this.props.changeLng(lng);
         return (
             <I18n>
-                {(t, {i18n}) => {
-                	console.log(lang);
-                	console.log(t('about.txt3',lang));
-                	return (
+                {(t, {i18n}) => (
                   <div className="page-about" id="e-aboutBox">
-                  	<Menus lng={lng}/>
+                  	<Menus lng={lng} changeLng={this.props.changeLng.bind(this)}/>
 										<div className="content">
 											<div className="bg-lf"><img src={bglf} alt="" /></div>
 											<div className="bg-rt"><img src={bgrt} alt="" /></div>
 											<div className="ct">
-												<h1>Trinity通过状态通道技术对NEO进行链下扩容。</h1>
-												<p>{t('about.txt3',lang)}</p>
-												<p>{t('about.txt4',lang)}</p>
-												<p>{t('about.txt5',lang)}</p>
-												<p>{t('about.txt6',lang)}</p>
-												<p>{t('about.txt7',lang)}</p>
-												<p>{t('about.txt8',lang)}</p>
-												<p>{t('about.txt9',lang)}</p>
-												<p>{t('about.txt10',lang)}</p>
-												
-												
-												
+												<h1>{t('about.txt1',lng)}<br />{t('about.txt2',lng)}</h1>
+												<p>{t('about.txt3',lng)}</p>
+												<p>{t('about.txt4',lng)}</p>
+												<p>{t('about.txt5',lng)}</p>
+												<p>{t('about.txt6',lng)}</p>
+												<p>{t('about.txt7',lng)}</p>
+												<p>{t('about.txt8',lng)}</p>
+												<p>{t('about.txt9',lng)}</p>
+												<p>{t('about.txt10',lng)}</p>
 											</div>
 										</div>
 										<Footer lng={lng} />
                   </div>
-                )
-                }}
+                )}
             </I18n>
         );
     }
