38684b440a3417b668488112c56f57155c736bdc
liangjunljlj <453021745@qq.com>
2018-03-29 18:05:37 +0800

    中英文

--- src/entries/about/components/root/index.js
+++ src/entries/about/components/root/index.js
@@ -9,6 +9,8 @@ import bgrt from "../../../../assets/images/6.png";
 import "./index.less";
 import { indexRemFun } from "../../../../utils/util";
 
+
+
 export default class Root extends PureComponent {
     constructor(props) {
         super(props);
@@ -25,30 +27,38 @@ export default class Root extends PureComponent {
     }
     render() {
         const { lng } = this.props;
-
+				let lang = window.i18n.language;
+				lang = "zh"
         return (
             <I18n>
-                {(t, {i18n}) => (
+                {(t, {i18n}) => {
+                	console.log(lang);
+                	console.log(t('about.txt3',lang));
+                	return (
                   <div className="page-about" id="e-aboutBox">
                   	<Menus lng={lng}/>
 										<div className="content">
 											<div className="bg-lf"><img src={bglf} alt="" /></div>
 											<div className="bg-rt"><img src={bgrt} alt="" /></div>
 											<div className="ct">
-												<h1>Trinity is an offchain<br /> scaling scheme for Neo</h1>
-												<p>As an off-chain scaling solution for blockchain using the state channels technology,</p>
-												<p>Trinity aims to achieve quick off-chain transfer of contract assets, </p>
-												<p>privacy preservation and high concurrency processing of DApp. </p>
-												<p>According to the current roadmap, </p>
-												<p>Trinity will realize high throughput and low cost transfer of NEO utxo and NEP-5 contract assets.</p>
-												<p>Together with the NEO on-chain scaling solution, </p>
-												<p>Trinity will provide all-round support for DApp in the NEO ecosystem. In the future, </p>
-												<p>Trinity also plans to support other blockchain projects in order to become a fully autonomous decentralized performance-enhancing network for the entire community.</p>
+												<h1>Trinity通过状态通道技术对NEO进行链下扩容。</h1>
+												<p>{t('about.txt3',lang)}</p>
+												<p>{t('about.txt4',lang)}</p>
+												<p>{t('about.txt5',lang)}</p>
+												<p>{t('about.txt6',lang)}</p>
+												<p>{t('about.txt7',lang)}</p>
+												<p>{t('about.txt8',lang)}</p>
+												<p>{t('about.txt9',lang)}</p>
+												<p>{t('about.txt10',lang)}</p>
+												
+												
+												
 											</div>
 										</div>
 										<Footer lng={lng} />
                   </div>
-                )}
+                )
+                }}
             </I18n>
         );
     }
